c3eefae7bad8d03659e6a94a47cc6fa80b77cc65
Ivanildo Lopes <ivanildo.lopes.jr@gmail.com>
2023-01-31 17:28:36 -0300

    fix test e2e product

--- src/infrastructure/api/__tests__/product.e2e.spec.ts
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -1,56 +1,65 @@
-import { app, sequelize } from "../express";
+import {app, sequelize} from '../express';
 import request from "supertest";
 
-describe('E2E test for product', () => {
-  beforeEach(async () => {
-    await sequelize.sync({ force: true });
-  });
+describe("E2E test for product", () => {
 
-  afterAll(async () => {
-    await sequelize.close();
-  });
+    beforeEach(async () => {
+        await sequelize.sync({ force: true });
+    })
 
-  it('should create a product', async () => {
-    const response = await request(app)
-      .post('/product')
-      .send({
-        name: 'Product 1',
-        price: 10,
-        description: 'Product 1 description',
-      });
+    afterAll(async () => {
+        await sequelize.close();
+    })
 
-    expect(response.status).toBe(200);
-    expect(response.body.name).toBe('Product 1');
-    expect(response.body.price).toBe(10);
-    expect(response.body.description).toBe('Product 1 description');
-  });
 
-  it('should not create a product', async () => {
-    const response = await request(app).post('/product').send({
-      name: 'john',
+    it("should create a product", async () => {
+        const response = await request(app)
+        .post("/product")
+        .send({
+            id: "123",
+            name: "Product 1",
+            price: 10,
+        });
+        expect(response.status).toBe(200);
+        expect(response.body.name).toBe("Product 1");
+        expect(response.body.price).toBe(10);
     });
-    expect(response.status).toBe(500);
-  });
 
-  it('should list all products', async () => {
-    const response = await request(app)
-      .post('/product')
-      .send({
-        name: 'Product 1',
-        price: 10,
-        description: 'Product 1 description',
-      });
-    expect(response.status).toBe(200);
-    const response2 = await request(app)
-      .post('/product')
-      .send({
-        name: 'Product 2',
-        price: 20,
-        description: 'Product 2 description',
-      });
-    expect(response2.status).toBe(200);
-    const response3 = await request(app).get('/product');
-    expect(response3.status).toBe(200);
-    expect(response3.body.length).toBe(2);
-  });
-});
+    it("should not create a product", async () => {
+        const response = await request(app)
+        .post("/product")
+        .send({
+            name: "Product 1"
+        });
+        expect(response.status).toBe(500);
+    });
+
+    it("should list all products", async () => {
+        const response = await request(app)
+        .post("/product")
+        .send({
+            id: "123",
+            name: "Product 1",
+            price: 10,
+        });
+        expect(response.status).toBe(200);
+        const response2 = await request(app)
+        .post("/product")
+        .send({
+            id: "124",
+            name: "Product 2",
+            price: 20,
+        });
+        expect(response2.status).toBe(200);    
+        const listResponse = await request(app).get("/product").send();
+        expect(listResponse.status).toBe(200);
+        expect(listResponse.body.products.length).toBe(2);
+        const product = listResponse.body.products[0];
+        expect(product.name).toBe("Product 1");
+        expect(product.price).toBe(10);
+        const product2 = listResponse.body.products[1];
+        expect(product2.name).toBe("Product 2");
+        expect(product2.price).toBe(20);
+    });
+
+}) 
